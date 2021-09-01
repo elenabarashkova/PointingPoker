3f30bba7f5b05f9922d61d0563cf8a54da72737a
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-01 23:14:09 +0300

    feat: set-up webpack & basic folder system

---
+++ src/redux/actions.ts
@@ -0,0 +1,6 @@
+// import { PayloadAction } from '@reduxjs/toolkit';
+
+// export const exampleAction = (testInfo: string): PayloadAction<string> => ({
+//   type: TEST_NAME,
+//   payload: testInfo,
+// });
