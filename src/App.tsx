28eef1e702d04618d10ea8fe388ec7af22617cdc
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-01 23:14:09 +0300

    feat: set-up webpack & basic folder system

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React, { FunctionComponent, ReactElement } from 'react';
+import { Button } from '@material-ui/core';
+
+export const App: FunctionComponent = (): ReactElement => (
+  <Button color="primary">Hello super team!</Button>
+);
---
+++ webpack.config.ts
@@ -0,0 +1,84 @@
+import path from "path";
+import { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
+import history = require('connect-history-api-fallback');
+
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    port: 8080,
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    publicPath: "/",
+  },
+};
+
+const webpackConfig = ({ development }): Configuration => ({
+  entry: "./src/index.tsx",
+  mode: development ? 'development' : 'production',
+  devtool: !development ? 'inline-source-map' : false,
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: "/",
+    filename: "build.js",
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|mp3)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new CopyPlugin({
+      patterns: [{
+        from: './src/assets',
+        to: 'assets',
+        noErrorOnMissing: true,
+      }],
+    }),
+    new ESLintPlugin({ extensions: ['ts', 'js', 'tsx', 'jsx'] }),
+  ],
+  ...devServer(development)
+});
+
+export default webpackConfig;
