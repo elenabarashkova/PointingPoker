40c8b741c028ab82ffb5f1b2a4835dc399807907
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-02 13:34:54 +0300

    feat: add basic express-socket setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,29 @@
 import * as express from 'express';
+import * as path from 'path';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
-const http = require('http').Server(app);
-app.get('/', function(req, res) {
-  res.sendfile('index.html');
+const http = createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '') });
 });
 
-http.listen(3000, function() {
+const io = new Server(http);
+
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect',  () => {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+
+
