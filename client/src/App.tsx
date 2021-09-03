df82b85f7c7a075888799e7f62c089d46b54416e
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-03 16:00:22 +0300

    refactor: combine 2 button components into one

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,4 +2,5 @@ import React, { FunctionComponent, ReactElement } from 'react';
 
 export const App: FunctionComponent = (): ReactElement => (
   <h1>Hello</h1>
+  
 );
---
+++ client/src/components/shared/buttons/Button/index.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from 'react';
+import styles from './style.module.scss';
+
+interface ButtonProps {
+  content: string,
+  variant: string,
+  action(): void
+}
+
+const Button: React.FC<ButtonProps> = ({ content, variant, action }): ReactElement => (
+  <button
+    type="button"
+    className={[styles.btn, styles[`btn_${variant}`]].join(' ')}
+    onClick={action}
+  >
+    {content}
+  </button> 
+); 
+
+export default Button;
