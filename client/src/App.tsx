3d935dc4d86b5ca35f1354a2ab238708af59b842
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-03 11:09:44 +0300

    fix: fix minor errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent, ReactElement } from 'react';
 import { Button } from '@material-ui/core';
 
-console.log('lalala')
-
 export const App: FunctionComponent = (): ReactElement => (
   <Button color="primary">Hello, my name is Nastya!</Button>
 
