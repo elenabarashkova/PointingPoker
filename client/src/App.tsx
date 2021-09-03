ba8b7ef7c35f3b7fc9854cada7a2535a8ed62ff5
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-03 15:03:27 +0300

    feat: add modal component p.1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
 import React, { FunctionComponent, ReactElement } from 'react';
 import { Button } from '@material-ui/core';
+import { Modal } from 'components/shared/Modal';
 
 export const App: FunctionComponent = (): ReactElement => (
-  <Button color="primary">Hello, my name is Nastya!</Button>
-
+  <Modal
+    openModalBtnTitle="Click to open modal"
+    modalTitle="This is modal title"
+    Component={<Button>Test</Button>}
+    actionBtnYesTitle="Confirm"
+    actionBtnYesOnClick={() => {
+      console.log('Yes');
+    }}
+    actionBtnNoTitle="Decline"
+    actionBtnNoOnClick={() => {
+      console.log('No');
+    }}
+  />
 );
