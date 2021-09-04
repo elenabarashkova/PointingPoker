2be3964ad8b13d2f579b919db25b8cedfa2fa7d3
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-04 17:56:57 +0300

    feat: add main page basic structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,6 @@
 import React, { FunctionComponent, ReactElement } from 'react';
-import { Button } from '@material-ui/core';
-import { Modal } from 'components/shared/Modal';
+import MainPage from './pages/MainPage';
 
 export const App: FunctionComponent = (): ReactElement => (
-  <Modal
-    openModalBtnTitle="Click to open modal"
-    modalTitle="This is modal title"
-    Component={<Button>Test</Button>}
-    yesBtnTitle="Confirm"
-    yesBtnOnClick={() => {
-      console.log('Yes');
-    }}
-    noBtnNoTitle="Decline"
-    noBtnNoOnClick={() => {
-      console.log('No');
-    }}
-  />
+  <MainPage />
 );
