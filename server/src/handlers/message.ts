3f1cf3ff22e682eb826817ba33d93866362489b1
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-05 04:04:04 +0300

    add readme.md

--- server/src/handlers/message.ts
+++ server/src/handlers/message.ts
@@ -1,6 +1,6 @@
 import { Socket } from "socket.io";
 import { addMessage } from "../actions/message";
-import { MESSAGE_WAS_SENDED, RECEIVE_MESSAGE } from "../events";
+import { MESSAGE_WAS_SENT, RECEIVE_MESSAGE } from "../events";
 import { store } from "../store";
 import { MessageData } from "../types/data";
 
@@ -8,6 +8,6 @@ export const sendMessageHandler =
   (socket: Socket) =>
   ({ roomId, text }: MessageData): void => {
     const message = addMessage(store, roomId, socket.id, text);
-    socket.emit(MESSAGE_WAS_SENDED, socket.id, message);
+    socket.emit(MESSAGE_WAS_SENT, socket.id, message);
     socket.to(roomId).emit(RECEIVE_MESSAGE, message);
   };
