c994b7c9ac025b0347212360cbeb4fc093b98725
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-05 20:34:17 +0300

    fix: mistakes after several merges  fix & minor refactor

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,16 +1,50 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import Switch from 'components/shared/Switch';
 import styles from './style.module.scss';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import {
+  CHOICE_NO, CHOICE_YES,
+  ROLE_MASTER,
+  ROLE_OBSERVER,
+  ROLE_USER,
+  SWITCH_TYPE_CHOICE,
+  SWITCH_TYPE_ROLE,
+} from '../../constants';
 
-const MainPage: React.FC = (): ReactElement => (
+const MainPage: React.FC = (): ReactElement => {
+  const [role, setRole] = useState(ROLE_USER);
+  const [choice, setChoice] = useState(CHOICE_NO);
 
-  <div className={styles.wrapper}>
-    <Header />
-    <Main />
-    <Footer />
-  </div>
-);
+  const handleRoleChange = () => {
+    const newRole = (role === ROLE_USER) ? ROLE_OBSERVER : ROLE_USER;
+    setRole(newRole);
+  };
+
+  const handleChoiceChange = () => {
+    const newChoice = (choice === CHOICE_NO) ? CHOICE_YES : CHOICE_NO;
+    setChoice(newChoice);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <Main />
+      <Footer />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100px',
+        justifyContent: 'space-between',
+      }}
+      >
+        <Switch type={SWITCH_TYPE_ROLE} status={role} onChange={handleRoleChange} />
+        <Switch type={SWITCH_TYPE_ROLE} status={ROLE_MASTER} />
+        <Switch type={SWITCH_TYPE_CHOICE} status={choice} onChange={handleChoiceChange} />
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;
