bdbe9f1891760f8229faec6c1ce44a2e814339b1
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-05 19:24:38 +0300

    feat: add initial validation of gameIdInput

--- client/src/components/RegisterSection/index.tsx
+++ client/src/components/RegisterSection/index.tsx
@@ -7,12 +7,15 @@ import styles from './style.module.scss';
 const RegisterSection: React.FC = (): ReactElement => {
   const [modalOpen, setModalOpen] = useState(false);
   const [isMaster, setMaster] = useState(false);
+  const [gameIdInput, setGameIdInput] = useState('');
+  const [gameIdValidation, setGameIdValidation] = useState('');
 
   const handleCloseModal = () => {
     setModalOpen(false);
   };
 
   const handleIdInput = (name, idValue) => {
+    setGameIdInput(idValue);
     // todo: store name: idValue
     // todo: set handleClickBtnUser - disabled if id is empty
   };
@@ -22,11 +25,17 @@ const RegisterSection: React.FC = (): ReactElement => {
     setModalOpen(true);
   };
       
-  const handleClickBtnUser = () => {
-    // todo: send server request if idValue matches
+  const handleClickBtnUser = (): void => {
+    if (!gameIdInput) {
+      setGameIdValidation('Fill in the field');
+      return;
+    }
+    const serverResponse = true; // todo: send server request if idValue matches and get response
     // todo: show spinner while waiting
-    // todo: if sucsess open modal
-    // todo: if fail - show error message on TextInput "Invalid room Id"
+    if (!serverResponse) {
+      setGameIdValidation('Invalid room Id');
+      return;
+    }
     setModalOpen(true);
   };
 
@@ -36,8 +45,16 @@ const RegisterSection: React.FC = (): ReactElement => {
         <p>Are you ready to be a scram master?</p>
         <Button content="start new game" variant="colored" action={handleClickBtnMaster} />
       </div>
+      <div>OR</div>
       <div className={styles.registrEl}>
-        <TextInput name="gameId" value="" label="" placeholder="game ID" error="" onChange={handleIdInput} />
+        <TextInput
+          name="gameId"
+          value=""
+          label=""
+          placeholder="game ID"
+          error={gameIdValidation}
+          onChange={handleIdInput}
+        />
         <Button content="connect" variant="colored" action={handleClickBtnUser} />
       </div>
       <RegisterForm isOpen={modalOpen} closeModal={handleCloseModal} isMaster={isMaster} />
