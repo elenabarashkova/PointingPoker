56846cb9fbea2872451a5f8b23af33766ceca0cd
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-07 17:01:21 +0300

    feat: add different styling to the header depending on the page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
+import Header from 'components/Header';
 import React, { FunctionComponent, ReactElement } from 'react';
 import MainPage from './pages/MainPage';
+import { Pages } from './types/page';
 
 export const App: FunctionComponent = (): ReactElement => (
   <MainPage />
