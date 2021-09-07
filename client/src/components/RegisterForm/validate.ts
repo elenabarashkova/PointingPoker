1a393c48f249ed29dff74dcdafe00f43d5a7bdb1
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-07 13:32:24 +0300

    add review comments

--- client/src/components/RegisterForm/validate.ts
+++ client/src/components/RegisterForm/validate.ts
@@ -1,6 +1,7 @@
 import { FIELDS_CONFIG } from 'components/RegisterForm/fields-config';
 
 export const validate = (fieldsState: Record<string, string | boolean>): Record<string, string> => (
+  // fieldsState: Record<string, string>
   FIELDS_CONFIG.reduce((acc, { name, required }) => {
     if (required && !fieldsState[name]) {
       acc[name] = 'Fill in the field';
@@ -8,3 +9,10 @@ export const validate = (fieldsState: Record<string, string | boolean>): Record<
     return acc;
   }, {})
 );
+
+export const validateField = (fieldName: string, value: string) => FIELDS_CONFIG.reduce((acc, { name, required }) => {
+  if (name === fieldName && required && !value) {
+    acc[name] = 'Fill in the field';
+  }
+  return acc;
+}, {});
