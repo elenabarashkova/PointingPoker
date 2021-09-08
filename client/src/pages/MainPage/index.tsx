33c2ef92ec79729fc2171c5e5d02ff338f152922
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-09 00:58:56 +0300

    feat: implement IconButton component

--- client/src/pages/MainPage/index.tsx
+++ client/src/pages/MainPage/index.tsx
@@ -1,19 +1,15 @@
-import { DeleteButton } from 'components/shared/buttons/DeleteButton';
-import { EditButton } from 'components/shared/buttons/EditButton';
-import React, { ReactElement } from 'react';
-import { Pages } from 'src/types/page';
-import Footer from '../../components/Footer';
-import Header from '../../components/Header';
-import Main from '../../components/Main(main-page)';
-import styles from './style.module.scss';
+import React, { ReactElement } from "react";
+import { Pages } from "src/types/page";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Main from "../../components/Main(main-page)";
+import styles from "./style.module.scss";
 
 const MainPage: React.FC = (): ReactElement => (
   <div className={styles.wrapper}>
     <Header page={Pages.main} />
     <Main />
     <Footer page={Pages.main} />
-    <EditButton onClick={() => console.log('dddd')} />
-    <DeleteButton onClick={() => console.log('dffffddd')} />
   </div>
 );
 
