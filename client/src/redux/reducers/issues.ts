21b80b859f1641ef8997720245a6e57ab8c82899
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-09 23:30:52 +0300

    feat: implement IssueItem, IssueCard, CreateIssue, AddButtons components

--- client/src/redux/reducers/issues.ts
+++ client/src/redux/reducers/issues.ts
@@ -1,23 +1,58 @@
-import { PayloadAction } from '@reduxjs/toolkit';
-import { SET_ISSUES } from '../action-types';
-import { Issue, Issues } from '../../types/issues';
+import { AnyAction } from "@reduxjs/toolkit";
+import { IssuesStore } from "../../types/issues";
+import {
+  ADD_ISSUE,
+  DELETE_ISSUE,
+  SEND_REQUEST,
+  SET_ERROR,
+  SET_ISSUES,
+  UPDATE_ISSUE
+} from "../action-types";
 
-export const initialState = {
+export const initialState: IssuesStore = {
   error: false,
   isLoading: false,
-  issues: [],
+  issues: {}
 };
 
 export const issues = (
-  state: Issues = initialState,
-  { type, payload }: PayloadAction<Array<Issue> | Issue | boolean>,
-): Issues => {
-  if (type === SET_ISSUES) {
-    return {
-      ...state,
-      issues: payload as Array<Issue>,
-    };
+  state: IssuesStore = initialState,
+  { type, payload }: AnyAction
+): IssuesStore => {
+  switch (type) {
+    case SEND_REQUEST:
+      return { ...state, isLoading: true, error: false };
+    case SET_ERROR:
+      return { ...state, isLoading: false, error: true };
+    case SET_ISSUES:
+      return {
+        ...state,
+        issues: { ...payload },
+        isLoading: false,
+        error: false
+      };
+    case ADD_ISSUE:
+      return {
+        ...state,
+        issues: { ...state.issues, [payload.issueId]: payload.issue },
+        isLoading: false,
+        error: false
+      };
+    case UPDATE_ISSUE:
+      return {
+        ...state,
+        issues: { ...state.issues, [payload.issueId]: payload.issue },
+        isLoading: false,
+        error: false
+      };
+    case DELETE_ISSUE:
+      return {
+        ...state,
+        issues: { ...state.issues, [payload.issueId]: payload.issue },
+        isLoading: false,
+        error: false
+      };
+    default:
+      return state;
   }
-
-  return state;
 };
