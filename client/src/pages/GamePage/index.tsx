ce8e7f7bd5b5fd9483655f20dd61ba5d15c5911a
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-09 13:29:51 +0300

    refactor: adjust votingCard scss file

--- client/src/pages/GamePage/index.tsx
+++ client/src/pages/GamePage/index.tsx
@@ -1,13 +1,18 @@
 import React, { ReactElement } from 'react';
 import { Pages } from 'src/types/page';
-import styles from './style.module.scss';
+import VotingCardsField from 'components/VotingCardsField';
+import { ScoreType } from 'src/types/room';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import styles from './style.module.scss';
 
 const GamePage: React.FC = (): ReactElement => (
   <div className={styles.wrapper}>
     <Header page={Pages.game} />
     <h2>GamePage</h2>
+    <VotingCardsField scoreType={ScoreType.storyPoint} number={6} />
+    <VotingCardsField scoreType={ScoreType.calories} number={6} />
+    <VotingCardsField scoreType={ScoreType.size} number={6} />
     <Footer page={Pages.game} />
   </div>
 );
