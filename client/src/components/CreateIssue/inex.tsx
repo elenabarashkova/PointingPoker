f84c09cbbbf25d53e729e05559a2b7aae231003c
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-10 22:56:12 +0300

    refactor: add action.ts

--- client/src/components/CreateIssue/inex.tsx
+++ client/src/components/CreateIssue/inex.tsx
@@ -1,11 +1,11 @@
-import { AddButton } from 'components/shared/buttons/AddButton';
-import React from 'react';
-import { CreateIssueProps } from 'src/types/issues';
-import styles from './style.module.scss';
+import { AddButton } from "components/shared/buttons/AddButton";
+import React from "react";
+import { CreateIssueProps } from "src/types/issues";
+import styles from "./style.module.scss";
 
 export const CreateIssue: React.FC<CreateIssueProps> = ({ addBtnAction }) => (
   <div className={styles.createIssue}>
-    <div className={styles.title}>Crete new Issue</div>
+    <div className={styles.title}>Create new Issue</div>
     <AddButton onClick={addBtnAction} />
   </div>
 );
