a13eacd4a773cbea4c5c08b00d4d89de2c52dd08
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-11 01:09:54 +0300

    feat: add CreateIssueModal component

--- client/src/redux/action-types.ts
+++ client/src/redux/action-types.ts
@@ -1,18 +1,18 @@
-export const SET_USERS = "SET_USERS";
-export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const SET_USERS = 'SET_USERS';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
-export const SET_ROOM_ID = "SET_ROOM_ID";
-export const SET_IS_ROOM_VALID = "SET_IS_ROOM_VALID";
-export const SET_GAME_STATUS = "SET_GAME_STATUS";
+export const SET_ROOM_ID = 'SET_ROOM_ID';
+export const SET_IS_ROOM_VALID = 'SET_IS_ROOM_VALID';
+export const SET_GAME_STATUS = 'SET_GAME_STATUS';
 
-export const SET_ALL_GAME_SETTINGS = "SET_ALL_GAME_SETTINGS";
+export const SET_ALL_GAME_SETTINGS = 'SET_ALL_GAME_SETTINGS';
 
-export const SET_MESSAGES = "SET_MESSAGES";
+export const SET_MESSAGES = 'SET_MESSAGES';
 
-export const SEND_REQUEST = "SEND_REQUEST";
-export const SET_ERROR = "SET_ERROR";
+export const SEND_REQUEST = 'SEND_REQUEST';
+export const SET_ERROR = 'SET_ERROR';
 
-export const SET_ISSUES = "SET_ISSUES";
-export const ADD_ISSUE = "ADD_ISSUE";
-export const UPDATE_ISSUE = "UPDATE_ISSUE";
-export const DELETE_ISSUE = "DELETE_ISSUE";
+export const SET_ISSUES = 'SET_ISSUES';
+export const ADD_ISSUE = 'ADD_ISSUE';
+export const UPDATE_ISSUE = 'UPDATE_ISSUE';
+export const DELETE_ISSUE = 'DELETE_ISSUE';
