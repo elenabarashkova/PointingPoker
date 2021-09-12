a7bb88b96e157c08e6320afac053a0210ed615fb
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-12 20:33:50 +0300

    feat: add marking nonactive user

--- client/src/components/shared/UserCard/index.tsx
+++ client/src/components/shared/UserCard/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { User, UserRole } from 'src/types/user';
+import { User, UserRole, UserStatus } from 'src/types/user';
 import { ElementSize } from 'src/types/additional';
 import KickButton from '../buttons/KickButton';
 import UserIco from '../UserIco';
@@ -19,24 +19,25 @@ const UserCard: React.FC<UserCardProps> = ({
   // если статус не UserStatus.active, перекрасить в серый цвет
 
   const { 
-    name, role, jobPosition, image, 
+    name, role, jobPosition, image, status,
   } = user;
   const [firstName, lastName] = name.split(' ');
 
   const isUserCurrentUser = (id === currentUserId);
 
   return (
-    <div className={`${styles.userCard} ${styles[size]} ${role === UserRole.master && styles.master}`}>
+    <div className={`${styles.userCard} ${styles[size]} ${role === UserRole.master && styles.master} ${status !== UserStatus.active && styles.nonActiveUser}`}>
       {(role === UserRole.master && size === ElementSize.big) 
       && <img src={crown} alt="crown" className={styles.crown} />}
       <div className={styles.wrapper}>
-        <UserIco firstName={firstName} lastName={lastName} imgSrc={image} size={size} />
+        <UserIco firstName={firstName} lastName={lastName} imgSrc={image} size={size} userStatus={status} />
         <div className={styles.userInfo}>
           {isUserCurrentUser && <span className={styles.currentUserInfo}>It is you</span>}
+          {status !== UserStatus.active && <span className={styles.noActiveStatus}>User is not active</span>}
           <p className={styles.userName}>{name}</p>
           <p className={styles.jobPosition}>{jobPosition}</p>
         </div>
-        {(!isUserCurrentUser && role !== UserRole.master) && <KickButton userId={id} />}
+        {(!isUserCurrentUser && role !== UserRole.master && status === UserStatus.active) && <KickButton userId={id} />}
       </div>
     </div>
   );
