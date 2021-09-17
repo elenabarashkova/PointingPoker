9a3b892a24bdfbf1d98b7cbbaaceb25cd4deef4f
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-17 11:21:09 +0300

    fix usercard styles

--- client/src/components/shared/UserCard/index.tsx
+++ client/src/components/shared/UserCard/index.tsx
@@ -5,6 +5,8 @@ import KickButton from '../buttons/KickButton';
 import UserIco from '../UserIco';
 import styles from './style.module.scss';
 import crown from './crown.svg';
+import observer from './observer.svg';
+import player from './player.svg';
 
 interface UserCardProps {
   user: User;
@@ -16,33 +18,51 @@ interface UserCardProps {
 const UserCard: React.FC<UserCardProps> = ({
   user, id, currentUserId, size, 
 }): ReactElement => {
-  // если статус не UserStatus.active, перекрасить в серый цвет
-
   const { 
     name, role, jobPosition, image, status,
   } = user;
   const [firstName, lastName] = name.split(' ');
 
   const notCurrentUser = !(id === currentUserId);
-  const notMaster = role !== UserRole.master;
+  const isMaster = role === UserRole.master;
+  const isPlayer = role === UserRole.player;
   const userIsActive = status === UserStatus.active;
+  const isCardBig = size === ElementSize.big;
 
-  const canUserBeKicked = notCurrentUser && notMaster && userIsActive;
+  const canUserBeKicked = notCurrentUser && !isMaster && userIsActive;
+
+  const userRoleInfo: ReactElement = (isMaster) 
+    ? (
+      <div className={styles.roleInfo}>
+        <img src={crown} alt="crown" className={styles.roleIco} /> 
+        <span>Scram master</span>
+      </div>
+    )
+    : (isPlayer)
+      ? (
+        <div className={styles.roleInfo}>
+          <img src={player} alt="game controller" className={styles.roleIco} /> 
+          <span>Player</span>
+        </div>
+      )
+      : (
+        <div className={styles.roleInfo}>
+          <img src={observer} alt="eyes" className={styles.roleIco} /> 
+          <span>Observer</span>
+        </div>
+      );
 
   return (
     <div className={`${styles.userCard} ${styles[size]} 
-      ${role === UserRole.master && styles.master} 
-      ${status !== UserStatus.active && styles.nonActiveUser}`}
+      ${isMaster && styles.master} 
+      ${!userIsActive && styles.nonActiveUser}`}
     >
-      {(role === UserRole.master && size === ElementSize.big) 
-      && <img src={crown} alt="crown" className={styles.crown} /> }
-      {(role === UserRole.master && size === ElementSize.big) 
-      && <span className={styles.scramMaster}>Scram master</span>}
+      {isCardBig && userRoleInfo}
       <div className={styles.wrapper}>
         <UserIco firstName={firstName} lastName={lastName} imgSrc={image} size={size} userStatus={status} />
         <div className={styles.userInfo}>
           {!notCurrentUser && <span className={styles.currentUserInfo}>It is you</span>}
-          {status !== UserStatus.active && <span className={styles.noActiveStatus}>User is not active</span>}
+          {/* {status !== UserStatus.active && <span className={styles.noActiveStatus}>User is not active</span>} */}
           <p className={styles.userName}>{name}</p>
           <p className={styles.jobPosition}>{jobPosition}</p>
         </div>
