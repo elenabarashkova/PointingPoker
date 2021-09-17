6a943020ba28c41100be9c0b8bed1f268305114d
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-17 16:09:22 +0300

    feat: add issues observing on setting page

--- client/src/pages/Settings/index.tsx
+++ client/src/pages/Settings/index.tsx
@@ -1,14 +1,27 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { Pages } from 'src/types/page';
 import MembersSection from 'components/MembersSection';
 import GameSection from 'components/GameSection';
+import { RootState } from 'src/redux/reducers';
 import styles from './style.module.scss';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { connect } from 'react-redux';
+import { Issues } from 'src/types/issues';
 
-const SettingsPage: React.FC = (): ReactElement => {
+export interface SettingsPageProps {
+  issues: Issues;
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = ({ issues }): ReactElement => {
   const [areSettingsCustom, setCustomSettings] = useState(false);
   const [areIssuesCreated, setCreatedIssues] = useState(false);
+  
+  useEffect(() => {
+    if (Object.keys(issues).length === 1) {
+      setCreatedIssues(true);
+    }
+  }, [issues]);
 
   const addCustomSettings = () => {
     setCustomSettings(true);
@@ -26,4 +39,8 @@ const SettingsPage: React.FC = (): ReactElement => {
   );
 };
 
-export default SettingsPage;
+const mapStateToProps = (state: RootState) => ({
+  issues: state.issuesStore.issues,
+});
+
+export default connect(mapStateToProps)(SettingsPage);
