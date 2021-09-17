77f44e5eee6199eff64b58132b4bf0e8e589ffe5
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-17 15:39:45 +0300

    feat: add gamesection and memrssection to settings page

--- client/src/pages/Settings/index.tsx
+++ client/src/pages/Settings/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { Pages } from 'src/types/page';
 import MembersSection from 'components/MembersSection';
 import GameSection from 'components/GameSection';
@@ -6,15 +6,24 @@ import styles from './style.module.scss';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-const SettingsPage: React.FC = (): ReactElement => (
-  <div className={styles.wrapper}>
-    <Header page={Pages.settings} />
-    <main className={styles.main}>
-      <GameSection />
-      <MembersSection />
-    </main>
-    <Footer page={Pages.settings} />
-  </div>
-);
+const SettingsPage: React.FC = (): ReactElement => {
+  const [areSettingsCustom, setCustomSettings] = useState(false);
+  const [areIssuesCreated, setCreatedIssues] = useState(false);
+
+  const addCustomSettings = () => {
+    setCustomSettings(true);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <Header page={Pages.settings} />
+      <main className={styles.main}>
+        <GameSection areIssuesCreated={areIssuesCreated} areSettingsCustom={areSettingsCustom} />
+        <MembersSection />
+      </main>
+      <Footer page={Pages.settings} />
+    </div>
+  );
+};
 
 export default SettingsPage;
