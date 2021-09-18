e60f3b5e2bf1eee063979d92ae0876bb23e949cf
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-18 16:54:30 +0300

    feat: create lobby page, implement user exit from the game session

--- client/src/pages/LobbyPage/index.tsx
+++ client/src/pages/LobbyPage/index.tsx
@@ -1,18 +1,44 @@
 import React, { ReactElement } from 'react';
 import { Pages } from 'src/types/page';
 import MembersSection from 'components/MembersSection';
-import styles from './style.module.scss';
-import Header from '../../components/Header';
+import useTypedSelector from 'src/hooks/useTypedSelector';
+import { UserRole } from 'src/types/user';
+import UserCard from 'components/shared/UserCard';
+import { ElementSize } from 'src/types/additional';
+import Button from 'components/shared/buttons/Button';
+import { connect } from 'react-redux';
+import { leaveRoomAction } from 'src/redux/actions/complexActions/leaveRoomAction';
 import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import styles from './style.module.scss';
 
-const LobbyPage: React.FC = (): ReactElement => (
-  <div className={styles.wrapper}>
-    <Header page={Pages.lobby} />
-    <main className={styles.main}>
-      <MembersSection />
-    </main>
-    <Footer page={Pages.lobby} />
-  </div>
-);
+export interface LobbyPageProps {
+  leaveRoom: CallableFunction;
+}
 
-export default LobbyPage;
+const LobbyPage: React.FC<LobbyPageProps> = ({ leaveRoom }): ReactElement => {
+  const roomUsers = useTypedSelector(({ users }) => users);
+  const roomId = useTypedSelector(({ game }) => game.roomId);
+  const [masterId, masterData] = Object.entries(roomUsers).filter(([, user]) => user.role === UserRole.master)[0];
+  const userId = useTypedSelector(({ currentUserId }) => currentUserId);
+
+  const handleExit = () => {
+    leaveRoom(roomId);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <Header page={Pages.lobby} />
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <UserCard user={masterData} id={masterId} currentUserId={userId} size={ElementSize.big} />
+          <Button content="exit" variant="colored" action={handleExit} />
+        </div>
+        <MembersSection />
+      </main>
+      <Footer page={Pages.lobby} />
+    </div>
+  );
+};
+
+export default connect(null, { leaveRoom: leaveRoomAction })(LobbyPage);
---
+++ client/src/redux/actions/complexActions/leaveRoomAction.ts
@@ -0,0 +1,21 @@
+import { Dispatch } from 'redux';
+import { createCommonNotificationAboutError } from 'src/helpers/commonNotifications';
+import { leaveRoom } from 'src/services/room/leaveRoom';
+import { CommonNotificationType, ImportantNotifications } from 'src/types/notifications';
+import { UserData } from 'src/types/user';
+import { setCommonNotification, setImportantNotification } from '../notifications';
+import { updateUser } from '../user';
+
+export const leaveRoomAction = (
+  roomId: string,
+) => async (dispatch: Dispatch): Promise<void> => {
+  try {
+    const userData = await leaveRoom(roomId) as UserData;
+  
+    dispatch(updateUser(userData));
+    dispatch(setImportantNotification(ImportantNotifications.userExitGame));
+  } catch (error) {
+    const notification = createCommonNotificationAboutError();
+    dispatch(setCommonNotification(notification));
+  }  
+};
