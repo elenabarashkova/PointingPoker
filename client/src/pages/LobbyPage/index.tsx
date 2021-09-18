42d4d0d6df4fbe9cea6fca25e82ff3b34c1e2e5d
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-18 12:11:30 +0300

    feat: add title component to all pages

--- client/src/pages/LobbyPage/index.tsx
+++ client/src/pages/LobbyPage/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import { Pages } from 'src/types/page';
 import MembersSection from 'components/MembersSection';
+import GameTitle from 'components/shared/GameTitle';
 import styles from './style.module.scss';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -9,6 +10,7 @@ const LobbyPage: React.FC = (): ReactElement => (
   <div className={styles.wrapper}>
     <Header page={Pages.lobby} />
     <main className={styles.main}>
+      <GameTitle editable={false} />
       <MembersSection />
     </main>
     <Footer page={Pages.lobby} />
