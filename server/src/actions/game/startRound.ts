a204c654bf66126e3fae2720b19177fde4da804a
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-20 11:35:34 +0300

    update server

--- server/src/actions/game/startRound.ts
+++ server/src/actions/game/startRound.ts
@@ -1,11 +1,15 @@
-import { Round, Store } from '../../types/room';
+import { Store } from '../../types/room';
 
 export const startRound = (
   roomId: string,
   issueId: string,
   store: Store
-): Round => {
+):  {roundIsActive: boolean, currentIssueId: string} => {
   const room = store[roomId];
-  room.currentRound = { issueId };
-  return room.currentRound;
+  room.roundIsActive = true;
+  room.currentIssueId = issueId;
+  return {
+    roundIsActive: room.roundIsActive,
+    currentIssueId: room.currentIssueId
+  };
 };
