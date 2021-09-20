16480b3d672d5828ff578cacd30e3f49f1eb8a5f
StacieKot <76011979+StacieKot@users.noreply.github.com>
2021-09-21 01:27:29 +0300

    fix: fix setFinalVoteRequest

--- client/src/redux/actions/complexActions/setFinalVoteAction.ts
+++ client/src/redux/actions/complexActions/setFinalVoteAction.ts
@@ -2,31 +2,31 @@ import { SetStateAction } from 'react';
 import { Dispatch } from 'redux';
 import {
   createCommonNotificationAboutError,
-  createCommonNotificationAboutFinalVote,
+  createCommonNotificationAboutFinalVote
 } from 'src/helpers/commonNotifications';
 import { setFinalVote } from 'src/services/voting/setFinalVote';
 import { CommonNotificationType } from 'src/types/notifications';
 import { setCommonNotification } from '../notifications';
 import { setFinalVoteAction } from '../voting';
 
-export const setFinalVoteRequest = (
-  roomId: string,
-  issueId: string,
-  finalVote: string,
-  setIsLoading: React.Dispatch<SetStateAction<boolean>>,
-) => async (dispatch: Dispatch): Promise<void> => {
-  try {
-    setIsLoading(true);
-    const votingData = await setFinalVote(roomId, issueId, finalVote);
-    setTimeout(() => {
+export const setFinalVoteRequest =
+  (
+    roomId: string,
+    issueId: string,
+    finalVote: string,
+    setIsLoading: React.Dispatch<SetStateAction<boolean>>
+  ) =>
+  async (dispatch: Dispatch): Promise<void> => {
+    try {
+      setIsLoading(true);
+      const votingData = await setFinalVote(roomId, issueId, finalVote);
       dispatch(setFinalVoteAction(votingData));
 
       const notification = createCommonNotificationAboutFinalVote(CommonNotificationType.success);
       dispatch(setCommonNotification(notification));
       setIsLoading(false);
-    }, 2000);
-  } catch (error) {
-    setIsLoading(false);
-    dispatch(createCommonNotificationAboutError());
-  }
-};
+    } catch (error) {
+      setIsLoading(false);
+      dispatch(createCommonNotificationAboutError());
+    }
+  };
