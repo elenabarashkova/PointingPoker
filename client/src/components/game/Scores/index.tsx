d126ae3267f5e0562a5d848ba3b3cbdf9a302cc5
elenabarashkova <lena.barashkova23@gmail.com>
2021-09-21 22:40:27 +0300

    feat: add cases for stop round

--- client/src/components/game/Scores/index.tsx
+++ client/src/components/game/Scores/index.tsx
@@ -1,17 +1,22 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import MembersList from './MembersList';
 import styles from './style.module.scss';
 import { RootState } from '../../../redux/reducers';
-import { Users } from '../../../types/user';
+import { UserRole, Users, UserStatus } from '../../../types/user';
 import { GameSettings } from '../../../types/room';
 import { UserVote } from '../../../types/voting';
+import { isMaster } from '../../../shared/isMaster';
+import { startRoundRequest } from '../../../redux/actions/complexActions/startRoundAction';
+import { stopRoundAction } from '../../../redux/actions/complexActions/stopRoundAction';
 
 interface ScoresProps {
   users: Users;
   gameSettings:GameSettings;
   votes: UserVote[];
   roundIsActive: boolean;
+  stopRound: CallableFunction;
+  roomId: string;
 }
 
 const Scores: React.FC<ScoresProps> = (
@@ -20,13 +25,49 @@ const Scores: React.FC<ScoresProps> = (
     gameSettings,
     votes,
     roundIsActive,
+    stopRound,
+    roomId,
   },
 ): ReactElement => {
-  const [isRoundStopped, sitRoundStoped] = useState(false);
+  const isUserMaster = isMaster();
+
+  const roomMembersData = Object.entries(users);
+  const activeMembers = roomMembersData.filter(([, { status }]) => (
+    (status === UserStatus.active || status === UserStatus.kicked)
+  ));
+  const master = activeMembers.filter(([, { role }]) => role === UserRole.master);
+  const observers = activeMembers.filter(([, { role }]) => role === UserRole.observer);
+  const players = activeMembers.filter(([, { role }]) => role === UserRole.player);
+
+  const gameMembers = {
+    master,
+    observers,
+    players,
+  };
+
+  useEffect(() => {
+    const { timer, autoFlip, masterAsPlayer } = gameSettings;
+    const playersQuantity = players.length + (masterAsPlayer ? 1 : 0);
+    const everyoneVoted = votes.length === playersQuantity;
+    // todo: make extra check for empty votes
+
+    const caseOne = roundIsActive && !timer && everyoneVoted;
+    const caseTwo = everyoneVoted && autoFlip;
+
+    if (caseOne || caseTwo) {
+      if (isUserMaster) {
+        stopRound(roomId);
+      }
+    }
+  });
 
   return (
     <div className={styles.scores}>
-      <MembersList isRoundActive={roundIsActive} />
+      <MembersList
+        isRoundActive={roundIsActive}
+        gameMembers={gameMembers}
+        hasActiveMembers={Boolean(activeMembers.length)}
+      />
     </div>
   );
 };
@@ -39,13 +80,14 @@ const mapStateToProps = (
     game,
   }: RootState,
 ) => {
-  const { currentIssueId, roundIsActive } = game;
+  const { currentIssueId, roundIsActive, roomId } = game;
   return ({
     users,
     gameSettings,
     votes: voting[currentIssueId].votes,
     roundIsActive,
+    roomId,
   });
 };
 
-export default connect(mapStateToProps)(Scores);
+export default connect(mapStateToProps, { stopRound: stopRoundAction })(Scores);
