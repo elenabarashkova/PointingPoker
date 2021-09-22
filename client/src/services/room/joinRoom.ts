7f15b6ac66738f224f27278c33f84f0871e66024
EkaterinaMosina <k.bedenko@mail.ru>
2021-09-22 09:51:21 +0300

    fix joinRoom, setNewUserAction

--- client/src/services/room/joinRoom.ts
+++ client/src/services/room/joinRoom.ts
@@ -2,14 +2,16 @@ import { RoomData } from 'src/types/room';
 import { User } from 'src/types/user';
 import { JOIN_ROOM, ResponseStatus, socket } from '../constants';
 
-export const joinRoom = (roomId: string, user: User): Promise<RoomData> => new Promise<RoomData>((resolve, reject) => {
+export const joinRoom = (
+  roomId: string, 
+  user: User,
+  ): Promise<RoomData | string> => new Promise<RoomData>((resolve, reject) => {
   socket.emit(JOIN_ROOM, { roomId, user }, ({ status, data, error }) => {
-    if (status === ResponseStatus.ok) {
+    if (status === ResponseStatus.ok 
+      || status === ResponseStatus.admit 
+      || status === ResponseStatus.notFound) {
       resolve(data);
       return;
-    }
-    if (status === ResponseStatus.notFound) {
-      // todo: показать сообщение 'комната не найдена'
     }
     reject(error);
   });
