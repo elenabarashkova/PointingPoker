da11ee34b9e1d9784534b3a804bfcb1e58c774dd
EkaterinaMosina <k.bedenko@mail.ru>
2021-10-04 22:15:41 +0300

    feat: add tests

---
+++ client/src/components/chatArea/ChatField/index.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ChatField from '.';
+
+// describe('chat', () => {
+//     test('chat should become opened/closed by chatButton click', () => {
+//         render(<ChatField />);
+//         const chatButton = 
+//     })
+// })
